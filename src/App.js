0b9dcc7d7ad1b0a533d065e1237830c52418656d
Roman <myshchyshyn_roman@ukr.net>
2025-01-13 15:38:06 +0200

    app.js routes and 404 page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import Carrousel from './components/Carrousel';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Mainpage from "./pages/Mainpage/Mainpage"
-
+import Categories from "./pages/Categories/Categories"
+import NotFound from "./pages/NotFound"
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Mainpage />}/>
+          <Route path="/categories" element={<Categories />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

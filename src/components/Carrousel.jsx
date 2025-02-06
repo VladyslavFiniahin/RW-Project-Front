abd5d1e388180ab7679fa381f7a2d183b5c69c3f
IlonaDembitska <dembitska3@gmail.com>
2025-02-06 12:10:41 +0200

    test

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -2,6 +2,29 @@ import { React, useState, useEffect } from "react";
 import styles from "./carrousel.module.css";
 
 const Carrousel = () => {
+    const recipes = [
+        { img: "/LastRecepies/1.png", name: "Pasta with mushrooms", link: "#" },
+        { img: "/LastRecepies/2.png", name: "Pancakes with fruits", link: "#" },
+        { img: "/LastRecepies/3.png", name: "Pasta with shrimps", link: "#" },
+        { img: "/LastRecepies/4.png", name: "Casseroles", link: "#" },
+        { img: "/LastRecepies/5.png", name: "Pancakes with eggplant", link: "#" },
+        { img: "/LastRecepies/6.png", name: "Fried potatoes", link: "#" },
+        { img: "/LastRecepies/7.png", name: "Oatmeal pancake", link: "#" },
+        { img: "/LastRecepies/8.png", name: "Salmon", link: "#" },
+        { img: "/LastRecepies/9.png", name: "Oatmeal", link: "#" },
+        { img: "/LastRecepies/10.png", name: "Cheese pancakes", link: "#" },
+        { img: "/LastRecepies/11.png", name: "Rice with vegetables", link: "#" },
+        { img: "/LastRecepies/12.png", name: "Vatrushky", link: "#" },
+        { img: "/LastRecepies/13.png", name: "Caesar", link: "#" },
+        { img: "/LastRecepies/14.png", name: "Grandmother's pancakes", link: "#" },
+        { img: "/LastRecepies/15.png", name: "Homemade pizza", link: "#" },
+        { img: "/LastRecepies/16.png", name: "Manty", link: "#" },
+        { img: "/LastRecepies/17.png", name: "Khachapuri", link: "#" },
+        { img: "/LastRecepies/18.png", name: "Homemade croissants", link: "#" },
+        { img: "/LastRecepies/19.png", name: "Greek salad", link: "#" },
+        { img: "/LastRecepies/20.png", name: "Granola with fruits", link: "#" },
+    ];
+    /*
     const [recipes, setRecipes] = useState([]);
     const [loading, setLoading] = useState(true);
 
@@ -24,7 +47,7 @@ const Carrousel = () => {
 
         fetchRecipes();
     }, []);
-
+*/
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const totalItems = recipes.length;
@@ -46,9 +69,9 @@ const Carrousel = () => {
         return items;
     };
     
-    if (loading) {
+    /*if (loading) {
         return <p>Loading recipes...</p>;
-    }
+    }*/
 
     return (
         <div className={styles.container}>
